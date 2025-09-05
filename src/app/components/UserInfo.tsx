fef1864424a1c2c375944e64730bc23d1b4337a0
roma564 <roma.gordiychuk.21@gmail.com>
2025-09-05 15:34:58 +0300

    avatar update

--- src/app/components/UserInfo.tsx
+++ src/app/components/UserInfo.tsx
@@ -1,14 +1,24 @@
 import { Avatar } from '@mui/material'
 import React from 'react'
 
+function getCookie(name: string): string | null {
+  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+  return match ? decodeURIComponent(match[2]) : null;
+}
+
 export default function UserInfo() {
+  const username = getCookie('username') || 'Guest';
+  const email = getCookie('email') || 'unknown@email.com';
+  const avatar = getCookie('avatar') || '/static/images/avatar/2.jpg';
+
   return (
     <div className='flex flex-row  items-center pr-5'>
         
-        <Avatar  alt="Travis Howard" src="/static/images/avatar/2.jpg" className='ml-2 mr-2'/>
+
+        <Avatar alt={username} src={avatar || 'error'} className='ml-2 mr-2' />
         <div className="flex flex-col">
-            <p>Username</p>
-            <p>example@email.com</p>
+            <p>{username}</p>
+            <p>{email}</p>
         </div>
        
     </div>
