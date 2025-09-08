aa04c06ece1c7a2c217db1f095c70b643cae760a
roma564 <roma.gordiychuk.21@gmail.com>
2025-09-08 15:40:05 +0300

    avatar feature

--- src/app/components/UserInfo.tsx
+++ src/app/components/UserInfo.tsx
@@ -1,26 +1,32 @@
-import { Avatar } from '@mui/material'
-import React from 'react'
-
-function getCookie(name: string): string | null {
-  const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
-  return match ? decodeURIComponent(match[2]) : null;
-}
+import { Avatar } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 
 export default function UserInfo() {
-  const username = getCookie('username') || 'Guest';
-  const email = getCookie('email') || 'unknown@email.com';
-  const avatar = getCookie('avatar') || '/static/images/avatar/2.jpg';
+  const [username, setUsername] = useState('Guest');
+  const [email, setEmail] = useState('unknown@email.com');
+  const [avatar, setAvatar] = useState('/static/images/avatar/2.jpg');
+
+  useEffect(() => {
+    const getCookie = (name: string): string | null => {
+      const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+      return match ? decodeURIComponent(match[2]) : null;
+    };
+
+    console.log(avatar)
+
+    setUsername(getCookie('username') || 'Guest');
+    setEmail(getCookie('email') || 'unknown@email.com');
+    setAvatar(getCookie('avatar') || '/static/images/avatar/2.jpg');
+
+  }, []);
 
   return (
-    <div className='flex flex-row  items-center pr-5'>
-        
-
-        <Avatar alt={username} src={avatar || 'error'} className='ml-2 mr-2' />
-        <div className="flex flex-col">
-            <p>{username}</p>
-            <p>{email}</p>
-        </div>
-       
+    <div className='flex flex-row items-center pr-5'>
+      <Avatar alt={username} src={avatar || 'error'} className='ml-2 mr-2' />
+      <div className="flex flex-col">
+        <p>{username}</p>
+        <p>{email}</p>
+      </div>
     </div>
-  )
+  );
 }
