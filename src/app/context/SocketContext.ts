afa335de73f0164faa03ecd0127dfc090c6c40bb
roma564 <roma.gordiychuk.21@gmail.com>
2026-01-26 20:34:22 +0200

    small refactor

--- src/app/context/SocketContext.ts
+++ src/app/context/SocketContext.ts
@@ -3,7 +3,7 @@ import { createContext } from 'react';
 import { io, Socket } from 'socket.io-client';
 
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : process.env.NEXT_PUBLIC_SERVER_BASE_URL;
+const URL = process.env.NEXT_PUBLIC_SERVER_BASE_URL;
 
 export const socket = io(URL);
 
